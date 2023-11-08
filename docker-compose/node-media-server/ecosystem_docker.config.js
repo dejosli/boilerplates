7bef121d71dbcf83db4351c699fedaf4ab94aef6
dejosli <bjoslishubho@gmail.com>
2023-11-08 11:16:49 +0600

    updated node-media-server config

--- docker-compose/node-media-server/ecosystem_docker.config.js
+++ docker-compose/node-media-server/ecosystem_docker.config.js
@@ -7,8 +7,6 @@ module.exports = {
       exec_mode: 'fork',
       watch: false,
       autorestart: true,
-      max_auto_restart: '12GB',
-      restart_delay: 0,
       time: true,
       log_type: 'format',
       log_date_format: 'DD-MM-YYYY HH:mm Z',
@@ -16,7 +14,6 @@ module.exports = {
       error_file: '/dev/null',
       pid_file: '/home/admin/.pm2/logs/nms.pid',
       log_file: '/home/admin/.pm2/logs/nms.log',
-      node_args: ['--max-old-space-size=13312'], // increase to 13GB
     },
   ],
 };
